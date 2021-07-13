b1c3df777c445af3933baa728ff8d39d5c760483
parrotshen <parrotshen@yahoo.com.tw>
2021-07-13 09:17:13 +0800

    using error function

--- debug/check.mk
+++ debug/check.mk
@@ -4,16 +4,16 @@ all: err
 	@echo "[1;33m$*[0m = $($*)"
 
 check:
-	@echo "[1;36mCC[0m = $(CROSS)$(CC)"
+	@echo "[1;36mCC[0m = $(CC)"
 	@echo "[1;36mCFLAGS[0m ="
 	@echo "$(CFLAGS)" | tr -s " " "\n"
 	@echo "[1;36mLDFLAGS[0m ="
 	@echo "$(LDFLAGS)" | tr -s " " "\n"
 
 
-ERR = $(if $(shell which $(CROSS)$(CC) 2> /dev/null), \
+ERR = $(if $(shell which $(CC) 2> /dev/null), \
            , \
-           $(error Cannot find $(CROSS)$(CC) !))
+           $(error Can not find $(CC) !))
 
 err: ; $(ERR)
 
